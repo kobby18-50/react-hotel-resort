3ee827373011ad9bc7d2d3ae6993004479f8419b
kobby18-50 <kobbyokai18@gmail.com>
2022-02-17 00:48:32 +0000

    setting up env

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space : "3g05ie3dfbh8",
-    accessToken : "IxRVcYc6fraa07WldH50od8wLKBOY04n1BApYRT5VUM"
+    space : process.env.REACT_APP_API_SPACE,
+    accessToken : process.env.REACT_APP_ACCESS_TOKEN
 })
